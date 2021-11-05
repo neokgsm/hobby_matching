9b52e065fc94bc00169ceb6b08d67043b25a4df1
neokgsm <79746975+neokgsm@users.noreply.github.com>
2021-11-05 17:28:21 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -1,15 +1,17 @@
 import streamlit as st
 st.write(st.session_state)
 
-if 'ss_list' not in st.session_state:
-  st.session_state.ss_list = []
-
-def update():
-  if 'check1' not in st.session_state.ss_list:
-    st.session_state.ss_list.append('check1')
-  elif 'check1' in st.session_state.ss_list:
-    st.session_state.ss_list.remove('check1')
-
+#if 'ss_list' not in st.session_state:
+  #st.session_state.ss_list = []
 
 check1 = st.checkbox('check1', key='check1')
 check2 = st.checkbox('check2', key='check2')
+check3 = st.checkbox('check3', key='check3')
+check4 = st.checkbox('check4', key='check4')
+check5 = st.checkbox('check5', key='check5')
+
+def show():
+  checked = [k for k, v in st.session_state.items() if v==True]
+  st.write(checked)
+
+st.button('Stat...', on_clicked=show)
