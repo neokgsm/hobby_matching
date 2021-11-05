f4d2c66f15f65bf97faa0a76c1ce300d14c4fb5b
neokgsm <79746975+neokgsm@users.noreply.github.com>
2021-11-05 11:24:24 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -5,7 +5,7 @@ if 'ss_list' not in st.session_state:
   st.session_state.ss_list = []
 
 def append():
-  ss_list.append("Clicked!")
+  st.session_state.ss_list.append("Clicked!")
 
 st.write()
 st.button('button', on_click=append)
