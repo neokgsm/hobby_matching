62e89fc632aeaf72527c4d348f5b61da908f35b4
neokgsm <79746975+neokgsm@users.noreply.github.com>
2021-11-05 16:37:49 +0900

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -6,9 +6,9 @@ if 'ss_list' not in st.session_state:
 
 def update():
   if 'check1' not in st.session_state.ss_list and check1:
-    st.session_state.ss_list.append('check1')
-  elif 'check1' in st.session_state.ss_list and not check1:
     st.session_state.ss_list.remove('check1')
+  elif 'check1' in st.session_state.ss_list and not check1:
+    st.session_state.ss_list.append('check1')
 
 
 check1 = st.checkbox('check1', on_change=update)
