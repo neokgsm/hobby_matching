7a93d2b6908d97602d399741e6d82b9b0e921afc
neokgsm <79746975+neokgsm@users.noreply.github.com>
2021-12-08 20:01:56 +0900

    Update hobby_matching.py

--- hobby_matching.py
+++ hobby_matching.py
@@ -131,7 +131,7 @@ if st.button('Search users') == True:
     #st.write(result)
     st.table(pd.DataFrame({
         'Nickname':[users[result[a]]['user_nickname'] for a in range(len(result))],
-        'Hobbies':[str(users[result[a]]['user_hobbies'])[1:-1].replace("'","") for a in range(len(result))],
+        'Hobbies':[str([h.name in for h in users[result[a]]['user_hobbies']])[1:-1].replace("'","") for a in range(len(result))],
         'Contact':[users[result[a]]['sns_acc'] for a in range(len(result))]
     }))
      
