5c277132f2ab71e1200ba5b95bf3175b1a84ef84
neokgsm <79746975+neokgsm@users.noreply.github.com>
2021-12-08 18:51:59 +0900

    Update hobby_matching.py

--- hobby_matching.py
+++ hobby_matching.py
@@ -2,19 +2,53 @@ import random
 import streamlit as st
 import pandas as pd
 
-hobbies = ['スポーツ', '語学', '音楽', '読書', '旅行', 'ゲーム']
+import anytree
+from anytree import Node, RenderTree
+
+
+hobbies = Node("趣味", parent=None)
+
+G1_sports = Node("スポーツ", parent=hobbies)
+G2_baseball = Node("野球", parent=G1_sports)
+G2_soccer = Node("サッカー", parent=G1_sports)
+G2_basketball = Node("バスケットボール", parent=G1_sports)
+G2_tennis = Node("テニス", parent=G1_sports)
+
+G1_langlearn = Node("語学", parent=hobbies)
+G2_english = Node("英語", parent=G1_langlearn)
+G3_toeic = Node("TOEIC", parent=G2_english)
+G3_toefl = Node("TOEFL", parent=G2_english)
+G2_french = Node("フランス語", parent=G1_langlearn)
+G2_chinese = Node("中国語", parent=G1_langlearn)
+
+G1_music = Node("音楽", parent=hobbies)
+G2_rock = Node("ロック", parent=G1_music)
+G2_jazz = Node("ジャズ", parent=G1_music)
+G2_hiphop = Node("ヒップホップ", parent=G1_music)
+
+G1_reading = Node("読書", parent=hobbies)
+G2_novel = Node("小説", parent=G1_reading)
+G2_comic = Node("マンガ", parent=G1_reading)
+
+G1_game = Node("ゲーム", parent=hobbies)
+G2_rpg = Node("RPG", parent=G1_game)
+G2_FPS = Node("FPS", parent=G1_game)
+
 name = ['Taro', 'Hanako', 'John', 'Emma', 'Yuki', 'Michael', 'Shun', 'Sakura', 'Ryo', 'Mizuki']
 
-def sample(n, max_hobby, name):
+
+
+def sample(n, max_hobby, hobbies, name):
     users = []
+    hobby_count = len(hobbies.leaves)
     for a in range(n):
-        random_num = random.sample(range(len(hobbies)), k=random.randint(1, max_hobby))
         nickname = f'{name[random.randint(1, len(name)-1)]}'
         users.append({'userID':a,
                       'user_nickname':nickname+f'#{a}',
-                      'user_hobbies':[hobbies[b] for b in random_num],
+                      'user_hobbies':set([hobbies.leaves[random.randint(1, hobby_count-1)] for _ in range(random_num)])}),
                       'sns_acc':f'@{nickname}_{a}'})
     return users
+
 
 
 def perfect_search(users, search_by):
@@ -67,7 +101,7 @@ def search_user(users, mode, hobby_selection):
 
 
 
-users = sample(10,5,name)
+users = sample(10,5,,hobbies,name)
 
 
 st.title('Hobby Matching')
